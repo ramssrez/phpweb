75aa559abaf6d74c7bc9416e8f46e67760be6366
Raul Ramirez Perez <ramssrez@gmail.com>
2023-11-04 12:18:27 +0100

    Cambio de dirección de la barra de navegación

--- pages/RRPCRUDActor.php
+++ pages/RRPCRUDActor.php
@@ -12,10 +12,10 @@
                 <li>
                     <a href="RRPAdminHome.php" id="logo">Cine Ciudad Puebla</a>
                 </li>
-                <li><a href="RRPCRUDPeliculas.html">CRUD Peliculas</a></li>
-                <li><a href="RRPCRUDCategoria.html">CRUS Categorias</a></li>
-                <li><a href="RRPCRUDDirector.html">CRUD Director</a></li>
-                <li><a href="RRPCRUDActor.html">CRUD Actor</a></li>
+                <li><a href="RRPCRUDPeliculas.php">CRUD Peliculas</a></li>
+                <li><a href="RRPCRUDCategoria.php">CRUS Categorias</a></li>
+                <li><a href="RRPCRUDDirector.php">CRUD Director</a></li>
+                <li><a href="RRPCRUDActor.php">CRUD Actor</a></li>
                 <li><a href="../config/logout.php">Cerrar Sesión</a></li>
             </ul>
         </nav>

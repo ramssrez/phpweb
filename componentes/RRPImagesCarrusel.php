05bfc10d4df5a3805526b21dc592954e5e10e074
Raul Ramirez Perez <ramssrez@gmail.com>
2023-11-18 16:28:12 +0100

    Creación del componente del carrusel y modificación de los archivos

---
+++ componentes/RRPImagesCarrusel.php
@@ -0,0 +1,16 @@
+        <div class="image-container">
+            <div class="image-galery">
+                <img src="../assets/images/12horas.jpeg" alt="Doce horas para sobrevivir">
+                <img src="../assets/images/chicaspesadas.jpeg" alt="Chicas Pesadas">
+                <img src="../assets/images/elhijodechucky.jpeg" alt="El hijo de chucky">
+                <img src="../assets/images/guerranovias.jpeg" alt="Guerra de Novias">
+                <img src="../assets/images/jovenesbrujas.jpeg" alt="Jovenes Brujas">
+                <img src="../assets/images/miPrimerBeso.jpeg" alt="Mi primer beso">
+                <img src="../assets/images/posdatateamo.jpeg" class="img-fluid rounded-top" alt="Posdata Te Amo">
+                <img src="../assets/images/sherk.jpeg" alt="Sherk">
+                <img src="../assets/images/sherk2.jpeg" alt="Sherk 2">
+                <img src="../assets/images/sherk3.jpeg" alt="Sherk 3">
+                <img src="../assets/images/supercool.jpeg" alt="Super Cool">
+                <img src="../assets/images/ylasrubias.jpeg" alt="Donde están las Rubias">
+            </div>
+        </div>

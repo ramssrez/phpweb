dbf69595fe914907bcfa626d4a05d500bec4a69c
Raul Ramirez Perez <ramssrez@gmail.com>
2023-11-26 12:24:34 +0100

    Integración de los script para generar e insertar datos en la base de datos del proyecto

---
+++ database/InsertarSexo.sql
@@ -0,0 +1,4 @@
+INSERT INTO `tblsexo` (`tipo`) VALUES 
+    ('Masculino'),
+    ('Femenino');
+INSERT INTO `db_peliculas_puebla`.`tblcliente` (`nombre`, `ap_paterno`, `ap_materno`, `rfc`, `curp`, `fecha_nacimiento`, `id_estado`, `id_sexo`) VALUES ('Jose', 'Sanchez', 'Hernandez', 'SAHJ850813MNL', 'SAHJ850813MNLEZZ73', '1985/10/13', '1', '1');

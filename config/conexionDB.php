23822b351f8fe212c94d3573b9b33f8c844875ee
Raul Ramirez Perez <ramssrez@gmail.com>
2023-11-28 22:15:01 +0100

    Integración de la base de datos externa

--- config/conexionDB.php
+++ config/conexionDB.php
@@ -1,6 +1,8 @@
 <?php
     $conn = new mysqli("localhost:3308","root","","db_peliculas_puebla");
     //$conn = new mysqli("localhost:3306","root","admin","db_peliculas_puebla");
+    //$conn = new mysqli("localhost","id21515347_admin_cine_puebla","Es1921023412*","id21515347_db_peliculas_puebla");
+
     if($conn->connect_error){
         die("Error en la conexión".$conn->connect_error);
     }
